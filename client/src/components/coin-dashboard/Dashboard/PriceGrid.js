99f4225003dc0305ba1cf82b982501439864b55c
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2019-04-20 23:59:28 +0530

    --update: displaying max price coin to invest from the fav list

--- client/src/components/coin-dashboard/Dashboard/PriceGrid.js
+++ client/src/components/coin-dashboard/Dashboard/PriceGrid.js
@@ -8,7 +8,37 @@ const PriceGrid = styled.div`
   grid-template-columns: repeat(5, 1fr); 
   grid-gap: 15px; 
   margin-top: 40px; 
+`;
+
+const Info = styled.div`
+  position: absolute;
+  background-color: white;
+  top: 80%;
+  font-size: 2rem;
+  color: black;
+  text-align: center;
+
 `
+
+const comparator = (prices) => {
+  if(prices.length!=0) {
+
+  let maxCoin = prices.reduce((accumulator, price) => {
+    let t = Object.keys(price)[0];
+    
+    let coin = {
+                value: Number(price[t]["USD"].PRICE),
+                name: price[t]["USD"].FROMSYMBOL
+                };
+
+    if((coin.value>accumulator.value) || Object.keys(accumulator).length==0){
+      return accumulator = coin;
+    }
+    return accumulator;
+  }, {})
+
+  return <p>{maxCoin.name} is the best coin to invest as compared to rest of your favourite coins.</p>;}
+}
 
 export default function () {
   return (
@@ -18,6 +48,7 @@ export default function () {
           {prices.map((price, index) => (
             <PriceTile key={`priceTile-${index}`} index={index} price={price}/>
           ))}
+          <Info>{comparator(prices)} </Info>
         </PriceGrid>
       )}
     </CryptoContext.Consumer>
